6987ae7576a71bd223d6fe44b399ebd43f8a01e4
Tu Truong <tutruong.se@gmail.com>
2023-12-14 18:31:48 +0700

    init MF

--- projects/app-shell/src/app/app.routes.ts
+++ projects/app-shell/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: AppComponent,
+        pathMatch: 'full'
+    },
+    {
+        path: 'flights',
+        loadChildren: () => import('mfe1/Module').then(m => m.FlightsModule)
+    },
+];
